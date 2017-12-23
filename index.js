47d5d07c4f728131fa4a9673da2107bc2f0ce247
Joel Tannas <jtannas@gmail.com>
2017-12-22 23:41:59 -0700

    now adds graph container and applies settings

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var DEFAULTS = {
   "container": {
     "width": "640px",
-    "height": "480px"
+    "height": "480px",
+    "background-color": "lightblue",
+    "border": "1px solid black"
   }
 };
 
-var drawBarChart = function(data,  options,  element){
+var drawBarChart = function(data,  options,  $element){
   var settings = $.extend(true, {}, DEFAULTS, options);
+  var $chart = $(document.createElement('div'));
+  $chart.css(settings.container);
+  $element.append($chart);
 };
+
+$(document).ready(function(){
+  drawBarChart([], {}, $('body'));
+});
