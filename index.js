637400adc3b7e2223676d2ba817a7d1883bea8d7
Joel Tannas <jtannas@gmail.com>
2017-12-24 13:11:35 -0700

    refactored to allow specification of chart DOM via JSON

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 var DEFAULTS = {
-  "container": {
-    "width": "640px",
-    "height": "480px",
-    "background-color": "lightblue",
-    "border": "1px solid black"
+  '$DOM': {
+    'type': 'div',
+    'css': {
+      'width': '640px',
+      'height': '480px',
+      'background-color': 'lightblue',
+      'border': '1px solid black'
+    },
+    'children': [
+      {
+        'type': 'p',
+        'html': 'Hello World!'
+      }
+    ]
   }
+};
+
+var createDOM = function(rootNodeJSON) {
+  var $DOM = $(document.createElement(rootNodeJSON.type));
+  $DOM.css(rootNodeJSON.css || {});
+  $DOM.html(rootNodeJSON.html || '');
+  (rootNodeJSON.children || []).forEach(function(child){
+    $DOM.append(createDOM(child));
+  });
+  return $DOM;
 };
 
 var drawBarChart = function(data,  options,  $element){
   var settings = $.extend(true, {}, DEFAULTS, options);
-  var $chart = $(document.createElement('div'));
-  $chart.css(settings.container);
+  var $chart = createDOM(settings.$DOM);
   $element.append($chart);
 };
 
