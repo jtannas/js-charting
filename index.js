28e3eac41e160892e9da68f58070b32a01bb4318
Joel Tannas <jtannas@gmail.com>
2017-12-26 16:16:25 -0700

    refactored to make a generic method for creating a DOM object from JSON

--- index.js
+++ index.js
@@ -1,37 +1,66 @@
-var DEFAULTS = {
-  '$DOM': {
+keyIntersection = function(object1, object2){
+  var sharedKeys = [];
+  for (var property in object1){
+    if (object1.hasOwnProperty(property) && object2.hasOwnProperty(property)){
+      sharedKeys.push(property);
+    }
+  }
+  return sharedKeys;
+};
+
+$.fn.extend({
+  setAttrs: function(attributesObject){
+    return this.each(function() {
+      var attributesCopy = $.extend(true, {}, attributesObject);
+      var $element = $(this);
+      var specialAttrFunctions = {
+        'log': console.log,
+        'css': $.fn.css.bind($element),
+        'html': $.fn.html.bind($element),
+        'innerHTML': $.fn.html.bind($element)
+      };
+      keyIntersection(specialAttrFunctions, attributesCopy).forEach(function(key){
+        specialAttrFunctions[key](attributesCopy[key]);
+        delete attributesCopy[key];
+      });
+      $(this).attr(attributesCopy);
+    });
+  }
+});
+
+var createDomObjectFromJson = function(domObjectJson, $parentElement) {
+  var $element = $(document.createElement(domObjectJson.type));
+  $element.setAttrs(domObjectJson.attributes || {});
+  (domObjectJson.children || []).forEach(function(child){
+    $element.append(createDomObjectFromJson(child));
+  });
+  if( $parentElement ) { $parentElement.append($element); }
+  return $element;
+};
+
+$(document).ready(function(){
+  var testData = {
     'type': 'div',
-    'css': {
-      'width': '640px',
-      'height': '480px',
-      'background-color': 'lightblue',
-      'border': '1px solid black'
+    'attributes': {
+      'log': 'The log function call works correctly',
+      'data-test': 'test1',
+      'css': {
+        'width': '640px',
+        'height': '480px',
+        'background-color': 'lightblue',
+        'border': '1px solid black'
+      }
     },
     'children': [
       {
         'type': 'p',
-        'html': 'Hello World!'
+        'attributes': {
+          'data': 'test2',
+          'innerHTML': 'Hello Paragraph!',
+          'title': 'paragraph title'
+        }
       }
     ]
-  }
-};
-
-var createDOM = function(rootNodeJSON) {
-  var $DOM = $(document.createElement(rootNodeJSON.type));
-  $DOM.css(rootNodeJSON.css || {});
-  $DOM.html(rootNodeJSON.html || '');
-  (rootNodeJSON.children || []).forEach(function(child){
-    $DOM.append(createDOM(child));
-  });
-  return $DOM;
-};
-
-var drawBarChart = function(data,  options,  $element){
-  var settings = $.extend(true, {}, DEFAULTS, options);
-  var $chart = createDOM(settings.$DOM);
-  $element.append($chart);
-};
-
-$(document).ready(function(){
-  drawBarChart([], {}, $('body'));
+  };
+  createDomObjectFromJson(testData, $('body'));
 });
