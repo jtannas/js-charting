4f609b72aa0f26bf51df24db25fdbc49580156db
Joel Tannas <jtannas@gmail.com>
2017-12-26 20:11:41 -0700

    replaced code in index files with calls to the draw-bar-chart module

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
 // requires jquery and create-dom-object-from-json.js
 
 $(document).ready(function(){
-  var testData = {
-    'type': 'div',
-    'attributes': {
-      'log': 'The log function call works correctly',
-      'data-test': 'test1',
-      'css': {
-        'width': '640px',
-        'height': '480px',
-        'background-color': 'lightblue',
-        'border': '1px solid black'
-      }
-    },
-    'children': [
-      {
-        'type': 'p',
-        'attributes': {
-          'data': 'test2',
-          'innerHTML': 'Hello Paragraph!',
-          'title': 'paragraph title'
-        }
-      }
-    ]
-  };
-  $('body').append(createDomObjectFromJson(testData));
+  drawBarChart([],{},$('body'));
 });
 
 
