a15a2e94134d5f32ff1ccb7d1ea1d753c6f5f20b
Joel Tannas <jtannas@gmail.com>
2017-12-26 20:16:40 -0700

    added list-based structure for bar charts

--- draw-bar-chart.js
+++ draw-bar-chart.js
@@ -1,10 +1,95 @@
 // requires jquery and create-dom-object-from-json.js
+var barChartBarUnits = {
+  'type': 'div',
+  'attributes': {
+    'class': 'units',
+    'innerHTML': '100mm'
+  }
+};
 
-var barChartDefaults = {};
+var barChartBarDescription =     {
+  'type': 'div',
+  'attributes': {
+    'class': 'description',
+    'innerHTML': 'Default description'
+  }
+};
+
+var barChartBar = {
+  'type': 'li',
+  'attributes': {
+    'class': 'bar',
+    'title': 'title',
+    'css': {
+      'height': '50%',
+      'background': '-webkit-linear-gradient(#999999  , #ffffff)'
+    }
+  },
+  'children': [
+    barChartBarUnits,
+    barChartBarDescription
+  ]
+};
+
+var barChartAxisLabel = {
+  'type': 'div',
+  'attributes': {
+    'class': 'axis-label',
+    'innerHTML': 'XXX%'
+  }
+};
+
+var barChartAxis = {
+  'type': 'li',
+  'attributes': {
+    'class': 'axis'
+  },
+  'children': [
+    barChartAxisLabel,
+    barChartAxisLabel,
+    barChartAxisLabel,
+    barChartAxisLabel,
+    barChartAxisLabel
+  ]
+};
+
+var barChartContents = {
+  'type': 'ul',
+  'attributes': {
+    'class': 'contents',
+    'css': {
+      'background-color': '#cccccc'
+    }
+  },
+  'children': [
+    barChartAxis,
+    barChartBar,
+    barChartBar,
+    barChartBar,
+    barChartBar
+  ]
+};
+
+var barChartContainer = {
+  'type': 'div',
+  'attributes': {
+    'class': 'bar_chart',
+    'css': {
+      'height': '500px',
+      'width': '500px',
+      'background-color': '#eeeeee'
+    }
+  },
+  'children': [barChartContents]
+};
+
+var barChartDefaults = {
+  '$dom': barChartContainer
+};
 
 var drawBarChart = function(data, options, element){
   var settings = $.extend(true, {}, barChartDefaults, options);
-  var $chart = createDomObjectFromJSON(settings);
+  var $chart = createDomObjectFromJson(settings.$dom);
   // TODO: apply data to chart
   // TODO: apply special options
   element.append($chart);
