d81c121945cb6b869933e4d97dbf0ea23d9820f0
Joel Tannas <jtannas@gmail.com>
2017-12-28 15:02:21 -0700

    revised json-to-dom conversion to be object oriented

--- create-dom-object-from-json.js
+++ create-dom-object-from-json.js
@@ -1,6 +1,5 @@
 // requires jquery
-
-keyIntersection = function(object1, object2){
+ownKeyIntersection = function(object1, object2){
   var sharedKeys = [];
   for (var property in object1){
     if (object1.hasOwnProperty(property) && object2.hasOwnProperty(property)){
@@ -21,7 +20,7 @@ $.fn.extend({
         'html': $.fn.html.bind($element),
         'innerHTML': $.fn.html.bind($element)
       };
-      keyIntersection(specialAttrFunctions, attributesCopy).forEach(function(key){
+      ownKeyIntersection(specialAttrFunctions, attributesCopy).forEach(function(key){
         specialAttrFunctions[key](attributesCopy[key]);
         delete attributesCopy[key];
       });
@@ -30,11 +29,20 @@ $.fn.extend({
   }
 });
 
-var createDomObjectFromJson = function(domObjectJson) {
-  var $element = $(document.createElement(domObjectJson.type));
-  $element.setAttrs(domObjectJson.attributes || {});
-  (domObjectJson.children || []).forEach(function(child){
-    $element.append(createDomObjectFromJson(child));
-  });
-  return $element;
+function DomObjectJson(options){
+  this.type = (options.type || 'div');
+  this.attributes = (options.attributes || {});
+  this.children = (options.children || []);
+}
+
+DomObjectJson.prototype = {
+  create: function(){
+    var $element = $(document.createElement(this.type));
+    $element.setAttrs(this.attributes);
+    (this.children).forEach(function(child){
+      var $childElement = new DomObjectJson(child).create();
+      $element.append($childElement);
+    });
+    return $element;
+  }
 };
--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // requires jquery and create-dom-object-from-json.js
 
 $(document).ready(function(){
-  drawBarChart([],{},$('body'));
+  var testData = {
+    'type': 'div',
+    'attributes': {
+      'log': 'The log function call works correctly',
+      'data-test': 'test1',
+      'css': {
+        'width': '640px',
+        'height': '480px',
+        'background-color': 'lightblue',
+        'border': '1px solid black'
+      }
+    },
+    'children': [
+      {
+        'type': 'p',
+        'attributes': {
+          'data': 'test2',
+          'innerHTML': 'Hello Paragraph!',
+          'title': 'paragraph title'
+        }
+      }
+    ]
+  };
+  var testDOM = new DomObjectJson(testData);
+  $('body').append(testDOM.create());
+  // drawBarChart([],{},$('body'));
 });
-
 
