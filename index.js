a1f5afb947ca1275b13ea8cbf16dd66ecbf3011d
Joel Tannas <jtannas@gmail.com>
2018-01-03 16:16:01 -0700

    removed HtmlSpec test data and call

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 // requires jquery and create-dom-object-from-json.js
 
 $(document).ready(function(){
-  // var testData = {
-  //   'type': 'div',
-  //   'attributes': {
-  //     'log': 'The log function call works correctly',
-  //     'data-test': 'test1',
-  //     'css': {
-  //       'width': '640px',
-  //       'height': '480px',
-  //       'background-color': 'lightblue',
-  //       'border': '1px solid black'
-  //     }
-  //   },
-  //   'children': [
-  //     {
-  //       'type': 'p',
-  //       'attributes': {
-  //         'data': 'test2',
-  //         'innerHTML': 'Hello Paragraph!',
-  //         'title': 'paragraph title'
-  //       }
-  //     }
-  //   ]
-  // };
-  // var testDOM = new HtmlSpec(testData);
-  // $('body').append(testDOM.createElement());
   var options = {
     BarChartContents: {
       'attributes': {
