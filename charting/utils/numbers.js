fa9fd13064de88d8f02da8bdda21d8e7b649e2be
Joel Tannas <jtannas@gmail.com>
2018-01-09 23:26:13 -0700

    commented the numbers utils

--- charting/utils/numbers.js
+++ charting/utils/numbers.js
@@ -1,7 +1,14 @@
 "use strict";
 
+/** Module Namespace */
 _c_.utils.numbers = {};
 
+/**
+ * Returns a given number constrained between a minimum and a maximum
+ * @param {Number} x - the number to constrain
+ * @param {Number} min - the minimum the result is allowed to be
+ * @param {Number} max - the maximum the result is allowed to be
+ */
 _c_.utils.numbers.constrainBetween = function(x, min, max){
   var result;
   if (x < min){
@@ -14,6 +21,12 @@ _c_.utils.numbers.constrainBetween = function(x, min, max){
   return result;
 };
 
+/**
+ * Converts a number in decimal to percentage
+ * @param {Number} decimal - the number to convert
+ * @param {Number} decimalPlaces - the number of digits after the decimal place
+    in the returned text
+ */
 _c_.utils.numbers.decimalToPercentageText = function(decimal, decimalPlaces){
   var percentage = decimal * 100;
   if (!isNaN(decimalPlaces)){
