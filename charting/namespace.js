a8841e6884aa03c83aab377498f458ca6caa9bf9
Joel Tannas <jtannas@gmail.com>
2018-01-09 22:04:22 -0700

    commented the namespace.js file

--- charting/namespace.js
+++ charting/namespace.js
@@ -1,9 +1,13 @@
 "use strict";
 
-/** Namespace & pre-made subsections for the library
- */
-var _c_ = {
-  utils: {},
-  dataObjects: {},
-  draw: {}
-};
+/** Namespace for the library */
+var _c_ = {};
+
+/** Namespace for library utility functions (e.g. sumIf) */
+_c_.utils = {};
+
+/** Namespace for library data objects (e.g. DataPoint) */
+_c_.dataObjects = {};
+
+/** Namespace for functions that draw charts from given data */
+_c_.draw = {};
