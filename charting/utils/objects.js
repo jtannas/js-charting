b5b4f170affd4ead4722999bb0ab8a8a85347372
Joel Tannas <jtannas@gmail.com>
2018-01-09 23:31:50 -0700

    commented objects utils

--- charting/utils/objects.js
+++ charting/utils/objects.js
@@ -1,7 +1,15 @@
 "use strict";
 
+/** Module Namespace */
 _c_.utils.objects = {};
 
+/**
+ * Given two objects, returns an array of property keys that _they_ share.
+ * This excludes keys that they inherited to prevent object defaults from being
+ * included.
+ * @param {Object} object1 - the first object
+ * @param {Object} object2 - the second object
+ */
 _c_.utils.objects.ownKeyIntersection = function(object1, object2){
   var sharedKeys = [];
   for (var property in object1){
