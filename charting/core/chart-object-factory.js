bb560f7f467a3df8d7b24374fff85aebf6ff2596
Joel Tannas <jtannas@gmail.com>
2018-01-10 00:05:51 -0700

    commented the chart object factory

--- charting/core/chart-object-factory.js
+++ charting/core/chart-object-factory.js
@@ -1,12 +1,16 @@
 "use strict";
 
+/** Extend the HtmlSpec into a ChartObject */
 _c_.ChartObject = Object.create(_c_.HtmlSpec);
 
+
 /** Creates a chart object with a given name, modifying it with the provides options.
- * @param {string} typeName - The name of the chart object in the CHART_DEFINITIONS
-    & CHART_SETTINGS & options
+ *
+ * @param {string} typeName - The name of the chart object in the _c_.HtmlSpec.definitions
+ *   and _c_.HtmlSpec.settings & options
  * @param {object} options - options[typeName] is 'folded into' the definition
-    of the returned object.
+ *   of the returned object.
+ * @returns {ChartObject}
  */
 _c_.ChartObject.create = function(typeName, options){
   var definitions = _c_.HtmlSpec.definitions[typeName] || {};
@@ -15,6 +19,17 @@ _c_.ChartObject.create = function(typeName, options){
   return this.createUsingExtendedOptions([definitions, settings, objOptions]);
 };
 
+
+/**
+ * Instance method for appending a new instance of a child of a given type,
+ * extending its css, and returning it to the caller for further modification.
+ *
+ * @param {string} typeName - The name of the chart object in the _c_.HtmlSpec.definitions
+ *   and _c_.HtmlSpec.settings & options
+ * @param {object} options - options[typeName] is 'folded into' the definition
+ *   of the returned object.
+ * @returns {ChartObject}
+ */
 _c_.ChartObject.prototype.addNewChild = function(typeName, options){
   var newObject = _c_.ChartObject.create(typeName, options);
   if (this[typeName + 'Css']){
