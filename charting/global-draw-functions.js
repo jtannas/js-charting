49fa686a9a58abbe9fd17ed7b43e5c4f38a82456
Joel Tannas <jtannas@gmail.com>
2018-01-09 23:00:12 -0700

    commented global draw functions

--- charting/global-draw-functions.js
+++ charting/global-draw-functions.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/** Provides a globally accessible alias for drawing bar charts */
 var drawBarChart = function(data, options, element){
   if (data instanceof _c_.dataObjects.Cluster){
     return _c_.draw.stackedVerticalBarChart(data, options, element);
